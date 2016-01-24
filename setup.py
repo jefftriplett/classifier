e238597965e4cc35a380eaf82d52a284efa5688f
Jeff Triplett <jeff.triplett@gmail.com>
2016-01-24 13:38:42 -0600

    PEP8

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-
 from setuptools import setup
 
-setup(name="classifier",
-	version="1.4",
-	description="Classify the files in your Downloads folder into suitable destinations.",
-	url="http://github.com/bhrigu123/classifier",
-	author="Bhrigu Srivastava",
-	author_email="captain.bhrigu@gmail.com",
-	license='MIT',
-	packages=["classifier"],
-	scripts=["bin/classifier"],
-	zip_safe=False)
+setup(
+    name="classifier",
+    version="1.4",
+    description="Classify the files in your Downloads folder into suitable destinations.",
+    url="http://github.com/bhrigu123/classifier",
+    author="Bhrigu Srivastava",
+    author_email="captain.bhrigu@gmail.com",
+    license='MIT',
+    packages=["classifier"],
+    scripts=["bin/classifier"],
+    zip_safe=False
+)
